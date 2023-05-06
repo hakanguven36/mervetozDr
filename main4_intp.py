6ba64b84770b38b3639306f5efcc78c2448fe0d6
ozitron <oguzfsen@gmail.com>
2023-05-06 11:39:41 +0300

    eto hesaplandı. Kodlar düzenlenmeli.

--- main4_intp.py
+++ main4_intp.py
@@ -17,21 +17,29 @@ datasetpath = "C:/Users/ozitron/Desktop/kullanımda/"
 if os.path.exists(datasetpath) == False:
     datasetpath = "C:/Users/oguzfehmi.sen.TARIM/Desktop/veriler/"
 
-file = open(os.path.join(datasetpath, "df_tamamlandi.pcl"), "rb")
+file = open(os.path.join(datasetpath, "file_df.pcl"), "rb")
 df = pd.DataFrame(pcl.load(file))
 file.close()
 del(file)
 
+df.rename(columns={"Istasyon_No":               "istno",
+                   "GUNESLENME_SURESI_saat":    "n_sun",
+                   "MAKSIMUM_SICAKLIK_°C":      "T_max",
+                   "MINIMUM_SICAKLIK_°C":       "T_min",
+                   "ORTALAMA_NEM_%":            "RH_mean",
+                   "GUNLUK_ORTALAMA_HIZI_m_sn": "U_z"
+                   }, inplace=True)
+
 # Rüzgar ve nem tahminlerini atalım.
 df.drop(columns=["U_z_tahmin", "RH_mean_tahmin"], inplace=True)
 
-df_ruzgar_dolu = df.loc[df["U_z"].isnull() == False]
+df_ruzgar_dolu = df.loc[df["RH_mean"].isnull() == False]
 df_ruzgar_dolu.reset_index(drop=True, inplace=True)
 
 df_ruzgar_dolu = pd.DataFrame(df_ruzgar_dolu)
 
 # Her istasyonun 10 diğer istasyon ile ilişkisinden bir çıkarıma gidelim.
-istasyonlar = pd.DataFrame(df.groupby("istno").first().index)
+istasyonlar = pd.DataFrame(df_ruzgar_dolu.groupby("istno").first().index)
 
 corr = []
 for i,row in istasyonlar.iterrows():
@@ -42,11 +50,22 @@ for i,row in istasyonlar.iterrows():
         k_istno = istasyonlar.loc[t,"istno"]
         sag_gunes = df_ruzgar_dolu.loc[df_ruzgar_dolu["istno"] == k_istno]
         birlesik_gunes = sol_gunes.merge(sag_gunes, on="date", how="inner")
-        r2_score_arr.append(r2_score(birlesik_gunes["U_z_x"].to_list(), birlesik_gunes["U_z_y"].to_list()))
+        if len(birlesik_gunes.index) < 1000:
+            continue
+        r2 = r2_score(birlesik_gunes["RH_mean_x"].to_list(), birlesik_gunes["RH_mean_y"].to_list())
+        if r2 < 0.7:
+            continue
+        r2_score_arr.append(r2)
     r2_score_arr.sort(reverse=True)
     corr.append(r2_score_arr)
+    print(i)
+
 corr = pd.DataFrame(corr)
 corr["istno"] = istasyonlar["istno"]
 corr.set_index("istno", inplace=True, drop=True)
+corr = corr.iloc[:, 0:4]
+corr = corr.loc[corr[3].isnull() == False]
+
+
 
 corr.to_csv(os.path.join(datasetpath, "ruzgar istasyonlar arasi korelasyon.csv"), sep=";", decimal=".")
---
+++ main5_eto.py
@@ -0,0 +1,52 @@
+import datetime
+import os
+import pickle as pcl
+import numpy as np
+import pandas as pd
+from eto import ETo
+
+datasetpath = "C:/Users/ozitron/Desktop/kullanımda/"
+if os.path.exists(datasetpath) == False:
+    datasetpath = "C:/Users/oguzfehmi.sen.TARIM/Desktop/veriler/"
+
+file = open(os.path.join(datasetpath, "df_tamamlandi.pcl"), "rb")
+df = pd.DataFrame(pcl.load(file))
+file.close()
+del(file)
+
+df["n_sun"] = df["n_sun_tahmin"]
+df["T_max"] = df["T_max_tahmin"]
+df["T_min"] = df["T_min_tahmin"]
+df.drop(["n_sun_tahmin", "T_max_tahmin", "T_min_tahmin", "U_z_tahmin","RH_mean_tahmin"], axis=1, inplace=True)
+
+ist17886 = df.loc[df["istno"] == 17886]
+ist17886 = pd.DataFrame(ist17886)
+ist17886.drop(columns=["istno"], inplace=True)
+ist17886.set_index("date", inplace=True, drop=True)
+
+
+file = open(os.path.join(datasetpath, "file_ist.pcl"), "rb")
+ist_df = pd.DataFrame(pcl.load(file))
+file.close()
+del(file)
+ist_df.rename(columns={"coordx": "lat",
+                       "coordy": "lon",
+                       "height": "z_msl"
+                       }, inplace=True)
+
+row = ist_df.loc[ist_df["istno"] == 17886]
+
+
+et1 = ETo()
+z_msl = row.z_msl.squeeze()
+lat = row.lat.squeeze()
+lon = row.lon.squeeze()
+# TZ_lon = 173
+freq = 'D'
+
+et1.param_est(ist17886, freq, z_msl, lat, lon)
+mydataframe = et1.ts_param
+
+eto1 = et1.eto_fao()
+
+eto1.head()
